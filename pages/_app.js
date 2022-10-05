6536925687b10d26f6dc2ba7f9f0baa763aae8d5
bbbundy <colin.sprows@gmail.com>
2022-10-05 08:08:40 -0400

    105commit

--- pages/_app.js
+++ pages/_app.js
@@ -7,7 +7,7 @@ function MyApp() {
       <Box 
       paddingY={0} 
       paddingX={0}
-      px={{xs:0, sm:3, md:6, lg:9, xl:12}}
+      px={{xs:0, sm:0, md:6, lg:8, xl:10}}
       >
         <Home />
       </Box>
