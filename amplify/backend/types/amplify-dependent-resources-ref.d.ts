bbf8065e96529181e0a626bef9bfb1b347252506
bbbundy <colin.sprows@gmail.com>
2022-10-05 14:29:45 -0400

    105commit

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -4,5 +4,21 @@ export type AmplifyDependentResourcesAttributes = {
             "GraphQLAPIIdOutput": "string",
             "GraphQLAPIEndpointOutput": "string"
         }
+    },
+    "auth": {
+        "userPoolGroups": {
+            "AdminsGroupRole": "string",
+            "DefaultGroupRole": "string"
+        },
+        "uncoverseappnexte880bb60e880bb60": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string",
+            "CreatedSNSRole": "string"
+        }
     }
 }
