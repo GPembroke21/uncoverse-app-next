75513e3dc1f1955afd1f3bb98e2b1700b262c9d8
bbbundy <colin.sprows@gmail.com>
2022-11-04 13:08:57 -0400

    114 img original adbar before links

--- components/AdBar.js
+++ components/AdBar.js
@@ -1,11 +1,16 @@
 import React, { useEffect, useState } from "react"
+import Image from 'next/image'
 import ImageList from '@mui/material/ImageList'
 import ImageListItem from '@mui/material/ImageListItem'
 import ImageListItemBar from '@mui/material/ImageListItemBar'
 import { styled } from "@mui/system"
-import Image from "next/image"
+import { NoEncryption } from '@mui/icons-material'
+import { categoryList } from "../src/static/StaticVariables"
+import Link from 'next/link'
 
-const AdBarContainer = styled("div")(({ theme }) => ({ margin: "0rem 1rem -1rem 1rem" }))
+const AdBarContainer = styled("div")(({ theme }) => ({
+  margin: "0rem 1rem -1rem 1rem",
+}));
 
 export default function TitlebarBelowImageList() {
   const [horizontalScroll, setHorizontalScroll] = useState(0);
@@ -18,9 +23,9 @@ export default function TitlebarBelowImageList() {
     imageListEl?.addEventListener(
       "scroll",
       () => {
-        // console.log("position left is", imageListEl.scrollLeft);
-        // console.log("scroll width", imageListEl.scrollWidth);
-        // console.log("client width", imageListEl.clientWidth);
+        console.log("position left is", imageListEl.scrollLeft);
+        console.log("scroll width", imageListEl.scrollWidth);
+        console.log("client width", imageListEl.clientWidth);
         setHorizontalScroll(imageListEl?.scrollLeft);
         setScrollWidth(imageListEl?.scrollWidth);
         setClientWidth(imageListEl?.clientWidth);
@@ -31,64 +36,62 @@ export default function TitlebarBelowImageList() {
 
   return (
     <AdBarContainer>
-      <ImageList
-        id="imageList"
-        sx={{
-          width: "100%",
-          gridAutoFlow: "column",
-          gridTemplateColumns: "repeat(auto-fill,minmax(15em, 1fr)) !important",
-          gridAutoColumns: "minmax(15em, 1fr)",
-          overflowX: 'scroll',
-          maskImage:
-            scrollWidth === clientWidth
-              ? [""]
-              :
-              scrollWidth === (clientWidth + horizontalScroll)
-                ? [
-                  "linear-gradient(to left, black calc(100% - 48px), transparent 100%)",
-                ]
-                :
-                horizontalScroll > 0
-                  ? [
-                    "linear-gradient(to left, transparent 0%, black 48px, black calc(100% - 48px), transparent 100%)",
-                    "linear-gradient(to right, transparent 0%, black 48px, black calc(100% - 48px), transparent 100%)",
-                  ]
-                  :
-                  horizontalScroll === 0
-                    ? [
-                      "linear-gradient(to right, black calc(100% - 48px), transparent 100%)",
-                    ]
-                    : [""],
-          scrollbarWidth: "none",
-          "&::-webkit-scrollbar": {
-            display: "none"
-          }
-        }}
-        gap={10}
-        variant='standard'
-        rowHeight="auto"
+    <ImageList
+      id="imageList"
+      sx={{ 
+      width: "100%",
+      gridAutoFlow: "column",
+      gridTemplateColumns: "repeat(auto-fill,minmax(15em, 1fr)) !important",
+      gridAutoColumns: "minmax(15em, 1fr)",
+      overflowX:'scroll',
+      maskImage:
+      scrollWidth === clientWidth
+        ? [""]
+        :
+      scrollWidth === (clientWidth + horizontalScroll)
+        ? [
+            "linear-gradient(to left, black calc(100% - 48px), transparent 100%)",
+          ]
+        :
+      horizontalScroll > 0
+        ? [
+            "linear-gradient(to left, transparent 0%, black 48px, black calc(100% - 48px), transparent 100%)",
+            "linear-gradient(to right, transparent 0%, black 48px, black calc(100% - 48px), transparent 100%)",
+          ]
+        :
+      horizontalScroll === 0
+        ? [
+            "linear-gradient(to right, black calc(100% - 48px), transparent 100%)",
+          ]
+        : [""],
+      scrollbarWidth: "none",
+      "&::-webkit-scrollbar": {
+        display: "none"
+      }
+      }}
+      gap= {10}
+      variant= 'standard'
+      rowHeight= "auto"
       >
-        {itemData.map((item) => (
-          <a key={item.key} href={item.link} target={"_blank"}>
-            <ImageListItem style={{ height: '100%', width: "100%" }}>
-              <Image
-                src={item.img}
-                srcSet={item.img}
-                alt={item.title}
-                loading="lazy"
-                width='100%'
-                height='100%'
-                style={{ borderRadius: 10 }}
-              />
-              <ImageListItemBar
-                title={item.title}
-                subtitle={<span>{item.subtitle}</span>}
-                position="below"
-              />
-            </ImageListItem>
+      {itemData.map((item) => (
+        <ImageListItem key={item.key}>
+          <img
+            src={`${item.img}?w=248&fit=crop&auto=format`}
+            srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+            alt={item.title}
+            loading="lazy"
+            style={{borderRadius: 10}}
+          />
+          <a href={item.link} target={"_blank"}>
+          <ImageListItemBar
+            title={item.title}
+            subtitle={<span>{item.subtitle}</span>}
+            position="below"
+          />
           </a>
-        ))}
-      </ImageList>
+        </ImageListItem>
+      ))}
+    </ImageList>
     </AdBarContainer>
   );
 }
