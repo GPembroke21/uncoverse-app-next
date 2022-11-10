e8f8f1642b7061c66f38e5de47c485049615aa28
Gareth Pembroke <GPembroke21@gsb.columbia.edu>
2022-11-10 18:14:28 -0500

    update profile page and add error messages 11/10

--- pages/profile.js
+++ pages/profile.js
@@ -2,141 +2,126 @@ import { styled } from "@mui/system"
 import Button from "@mui/material/Button"
 import Grid from "@mui/material/Grid"
 import Typography from "@mui/material/Typography"
+import EditIcon from '@mui/icons-material/Edit';
 import Link from 'next/link'
 import TextField from "@mui/material/TextField"
 import { signOut } from '../src/auth/SignOut'
-import { useLoginContext } from "../components/ContextProvider"
 import React from 'react'
 import { Auth } from "aws-amplify"
+import { Box } from "@mui/material";
+import GetUser from "../src/auth/GetUser";
 
-const Main = styled(Grid)(({ theme }) => ({
-  maxHeight: '50em', color: "#ffffff", margin: "4vw 2em", flexDirection: "row",
-  justifyContent: "center", [theme.breakpoints.down("sm")]: {
-    flexDirection: "column",
-    justifyContent: "left", "> div": { marginLeft: 0, marginRight: 0, paddingRight: "60px" }
-  }
-}));
-
-const EditButton = styled(Button)(({ theme }) => ({
-  fontFamily: "Inter", fontSize: "0.8rem", fontWeight: "500", textAlign: "center", color: "white",
-  backgroundColor: "transparent", border: "1px solid white", borderRadius: "4px", padding: "0.46rem",
-  cursor: "pointer", margin: "12px 0px", width: "min(40vw, 200px)",
-  height: "55px", "&:hover": { border: "1px solid #dd00ff", color: "#dd00ff" }
-}));
-
-const SaveButton = styled(Button)(({ theme }) => ({
-  fontFamily: "Inter", fontSize: "0.8rem", fontWeight: "500", textAlign: "center", color: "white",
-  backgroundColor: "transparent", border: "1px solid white", borderRadius: "4px", padding: "0.46rem",
-  cursor: "pointer", margin: "40px 0px", height: "55px", "&:hover": { border: "1px solid #dd00ff", color: "#dd00ff" }
-}));
-
-const SignOutButton = styled(Button)(({ theme }) => ({
-  fontFamily: "Inter", fontSize: "0.8rem", fontWeight: "500", textAlign: "center", color: "white",
-  backgroundColor: "transparent", border: "1px solid white", borderRadius: "4px", padding: "0.46rem",
-  cursor: "pointer", margin: "0px 0px", height: "55px", "&:hover": { border: "1px solid red", color: "red" }
-}));
-
-const TextFields = styled(TextField)(({ theme }) => ({
-  caretColor: "white", '& label.Mui-focused': { color: 'white' },
-  '& .MuiInput-underline:after': { borderBottomColor: 'white' },
-  '& .MuiOutlinedInput-root': {
-    '& fieldset': { borderColor: 'white' },
-    '&:hover fieldset': { borderColor: 'white' },
-    '&.Mui-focused fieldset': { borderColor: 'white' },
-  },
-  "& .MuiInputLabel-root": { color: 'white' },
-}));
+const Main = styled(Grid)(({ theme }) => ({ maxHeight: '50em', color: "#ffffff", margin: "4vw 2em", flexDirection: "row", justifyContent: "center", [theme.breakpoints.down("sm")]: { flexDirection: "column", justifyContent: "left", "> div": { marginLeft: 0, marginRight: 0, paddingRight: "60px" } } }))
+const EditButton = styled(Button)(({ theme }) => ({ fontFamily: "Inter", fontSize: "0.8rem", fontWeight: "500", textAlign: "center", color: "white", backgroundColor: "transparent", border: "1px solid white", borderRadius: "4px", padding: "0.46rem", cursor: "pointer", margin: "12px 0px", width: "min(40vw, 200px)", height: "55px", "&:hover": { border: "1px solid #dd00ff", color: "#dd00ff" } }))
+const SaveButton = styled(Button)(({ theme }) => ({ fontFamily: "Inter", fontSize: "0.8rem", fontWeight: "500", textAlign: "center", color: "white", backgroundColor: "transparent", border: "1px solid white", borderRadius: "4px", padding: "0.46rem", cursor: "pointer", margin: "40px 0px", height: "55px", "&:hover": { border: "1px solid #dd00ff", color: "#dd00ff" } }))
+const SignOutButton = styled(Button)(({ theme }) => ({ fontFamily: "Inter", fontSize: "0.8rem", fontWeight: "500", textAlign: "center", color: "white", backgroundColor: "transparent", border: "1px solid white", borderRadius: "4px", padding: "0.46rem", cursor: "pointer", margin: "0px 0px", height: "55px", "&:hover": { border: "1px solid red", color: "red" } }))
+const TextFields = styled(TextField)(({ theme }) => ({ caretColor: "white", '& label.Mui-focused': { color: 'white' }, '& .MuiInput-underline:after': { borderBottomColor: 'white' }, '& .MuiOutlinedInput-root': { '& fieldset': { borderColor: 'white' }, '&:hover fieldset': { borderColor: 'white' }, '&.Mui-focused fieldset': { borderColor: 'white' } }, "& .MuiInputLabel-root": { color: 'white' } }))
 
 export default function Profile(props) {
-  let loginCreds = useLoginContext()
-  const [request, setRequest] = React.useState(() => null)
+  const [changed, setChanged] = React.useState(false)
+  const request = GetUser(changed)
+  const [editName, setEditName] = React.useState(() => false)
+  const [editEmail, setEditEmail] = React.useState(() => false)
+  const [editEmailConfirm, setEditEmailConfirm] = React.useState(() => false)
+  const [errorMsg, setErrorMsg] = React.useState(() => null)
+  const handleCloseAndSignOut = () => signOut()
+  const [email, setEmail] = React.useState(() => null)
 
-  const getFunction = React.useCallback(async () => {
-    try {
-      let user = await Auth.currentAuthenticatedUser();
-      // console.log(user)
-      setRequest(user)
-    } catch (error) {
-      // console.log("Error fetching u_deets:", error)
-      setRequest(() => null)
-    }
-  }, [])
-
-  React.useEffect(() => { getFunction(); return () => { } }, [getFunction])
+  React.useEffect(()=> {
+    if (!request || !request.attributes.email) return
+    setEmail(request.attributes.email)
+  }, [request])
 
   async function addAttribute(name) {
     try {
-      let result = await Auth.updateUserAttributes(user, { 'name': name });
-      console.log(result)
+      await Auth.updateUserAttributes(request, { 'name': name });
+      setEditName(false); setChanged(!changed); setErrorMsg(null)
     } catch (err) {
+      setErrorMsg(err.message)
       console.log('error adding attributes:', err)
     }
   }
 
-  const handleCloseAndSignOut = () => {
-    signOut();
-  };
+  async function updateUserEmail(newEmail) {
+    await Auth.updateUserAttributes(request, {'email': newEmail }).then(() => {
+      console.log('a verification code is sent to', newEmail);
+      setEditEmailConfirm(true); setErrorMsg(null); 
+    }).catch((e) => {
+      setErrorMsg(e.message)
+      console.log('failed with error 2... ', e);
+    });
+  }
+
+  async function verifyEmailValidationCode(code, email) {
+    try {
+      await Auth.verifyCurrentUserAttributeSubmit('email', code)
+      console.log('email verified');
+      setEditEmailConfirm(false)
+      setEditEmail(false)
+      setErrorMsg(null)
+      setEmail(email)
+    } catch (err) {
+      setErrorMsg(err.message)
+      console.log('failed with error 1...', code, err.message);
+    }
+  }
 
   const handleSubmit = event => {
     event.preventDefault()
-    // addAttribute('Gareth Pem')
+    if (editName) {
+      addAttribute(event.target[0].value)
+    } else if (editEmailConfirm) {
+      verifyEmailValidationCode(event.target[2].value, event.target[0].value)
+    } else if (editEmail) {
+      updateUserEmail(event.target[0].value)
+      console.log("changing email..", event.target[0].value)
+    }
   };
 
-
+  const handleEditName = () => { setEditEmail(false); setEditEmailConfirm(false); setEditName(!editName) }
+  const handleEditEmail = () => { setEditName(false); setEditEmail(!editEmail) }
 
   return (
     <form onSubmit={handleSubmit} style={{ overflow: "hidden", padding: "0 0", background: "transparent" }}>
       <Main container>
-        {/* <Grid marginRight="25px">
-          <Grid item position="relative" width="min(40vw, 200px)" height="min(40vw, 200px)" marginBottom="13px">
-            <Image src='/profile.svg' layout="fill" objectFit="contain" unoptimized={true} />
-          </Grid>
-          <Grid item>
-            <EditButton>Edit Avatar</EditButton>
-          </Grid>
-        </Grid> */}
-        {/* <Divider style={{backgroundColor: "#2e2e2e", width:"100%", height: "0.01px", marginTop: "4vw"}} /> */}
         <Grid alignItems="flex-start" flexDirection="column">
           <Grid item marginBottom={1} marginTop={0}>
-            <Typography>Display Name</Typography>
-          </Grid>
-          <Grid item>
-            <TextFields
-              placeholder="Guest"
-              fullWidth
-              required
-              inputProps={{ sx: { "&::placeholder": { color: "white" } } }}
-              sx={{ input: { color: 'white' } }}
-            />
+            <Typography onClick={() => console.log(email, request)}>Display Name  &nbsp; <EditIcon sx={{ fontSize: "1rem" }} onClick={handleEditName} /></Typography>
           </Grid>
           <Grid item marginBottom={1} marginTop={1}>
-            <Typography>UserName</Typography>
+            {(editName || !request || !request.attributes.name) ?
+              <TextFields placeholder="Enter name..." fullWidth required inputProps={{ sx: { "&::placeholder": { color: "white" } } }} sx={{ input: { color: 'white' } }} />
+              :
+              <Typography>{request.attributes.name}</Typography>
+            }
           </Grid>
-          <Grid item>
-            <TextFields
-              placeholder="@guest69"
-              fullWidth
-              required
-              inputProps={{ sx: { "&::placeholder": { color: "white" } } }}
-              sx={{ input: { color: 'white' } }}
-            />
+          <Grid item marginBottom={1} marginTop={3}>
+            <Typography>Email &nbsp; <EditIcon sx={{ fontSize: "1rem" }} onClick={handleEditEmail} /></Typography>
           </Grid>
           <Grid item marginBottom={1} marginTop={1}>
-            <Typography>Email</Typography>
+            {(editEmail || !request) ?
+              <TextFields placeholder="Enter email..." fullWidth required inputProps={{ sx: { "&::placeholder": { color: "white" } } }} sx={{ input: { color: 'white' } }} />
+              :
+              <Typography>{email}</Typography>
+            }
           </Grid>
-          <Grid item>
-            <TextFields
-              placeholder="guest69@gmail.com"
-              fullWidth
-              required
-              inputProps={{ sx: { "&::placeholder": { color: "white" } } }}
-              sx={{ input: { color: 'white' } }}
-            />
-          </Grid>
-          <Grid item>
-            <SaveButton type='submit' fullWidth>Save</SaveButton>
-          </Grid>
-          <Grid item>
+          {editEmailConfirm &&
+            <Grid item marginBottom={1} marginTop={1}>
+              <TextFields placeholder="Enter confirmation code..." fullWidth required inputProps={{ sx: { "&::placeholder": { color: "white" } } }} sx={{ input: { color: 'white' } }} />
+            </Grid>
+          }
+
+          {(editEmail || editEmailConfirm || editName) &&
+            <Grid item marginBottom={1} marginTop={1}>
+              <SaveButton type='submit' fullWidth>Save</SaveButton>
+            </Grid>
+          }
+          {errorMsg &&
+            <Grid item marginBottom={1} marginTop={1}>
+              <Typography sx={{ color: "#FF0000" }}> {errorMsg} </Typography>
+            </Grid>
+          }
+          <Grid item marginBottom={1} marginTop={3}>
             <Link href="/">
               <SignOutButton fullWidth onClick={handleCloseAndSignOut}>Sign Out</SignOutButton>
             </Link>
--- src/auth/SignIn.js
+++ src/auth/SignIn.js
@@ -4,105 +4,34 @@ import { Button, Grid, Link, Paper, TextField, Typography } from "@mui/material"
 import { styled } from "@mui/system";
 import ThemeProvider from "../../Theme";
 
-
 const SignInButton = styled(Button)(({ theme }) => ({
-    fontFamily: "Inter",
-    fontSize: "0.8rem",
-    fontWeight: "500",
-    textAlign: "center",
-    color: "white",
-    backgroundColor: "transparent",
-    border: "1px solid white",
-    borderRadius: "0.4rem",
-    padding: "0.46rem",
-    cursor: "pointer",
-    margin: "12px 0px",
-    "&:hover": {
-        border: "1px solid #dd00ff",
-        color: "#dd00ff",
-        backgroundColor: "transparent",
-    }
+    fontFamily: "Inter", fontSize: "0.8rem", fontWeight: "500", textAlign: "center", color: "white", backgroundColor: "transparent", border: "1px solid white",
+    borderRadius: "0.4rem", padding: "0.46rem", cursor: "pointer", margin: "12px 0px", "&:hover": { border: "1px solid #dd00ff", color: "#dd00ff", backgroundColor: "transparent" }
 }));
 
 const TextFields = styled(TextField)(({ theme }) => ({
-    caretColor: "white",
-    '& label.Mui-focused': { color: 'white' },
-    '& .MuiInput-underline:after': { borderBottomColor: 'white' },
-    '& .MuiOutlinedInput-root': {
-        '& fieldset': { borderColor: 'white' },
-        '&:hover fieldset': { borderColor: 'white' },
-        '&.Mui-focused fieldset': { borderColor: 'white' }
-    },
-    "& .MuiInputLabel-root": { color: 'white' },
+    caretColor: "white", '& label.Mui-focused': { color: 'white' }, '& .MuiInput-underline:after': { borderBottomColor: 'white' }, '& .MuiOutlinedInput-root': {
+        '& fieldset': { borderColor: 'white' }, '&:hover fieldset': { borderColor: 'white' }, '&.Mui-focused fieldset': { borderColor: 'white' }
+    }, "& .MuiInputLabel-root": { color: 'white' }
 }));
-
-async function signIn(props) {
-    try {
-        const user = await Auth.signIn(props.un, props.pw)
-    } catch (error) {
-        console.log('error signing in', error);
-    }
-}
-
-async function forgotPd(props) {
-    try {
-        await Auth.forgotPassword(props.un)
-    } catch (error) {
-        console.log('error with forgot password', error);
-    }
-}
-
-async function submitForgotPassword(props) {
-    try {
-        Auth.forgotPasswordSubmit(props.un, props.code, props.new_pw)
-    } catch (error) {
-        console.log('error forgot password submit', error);
-    }
-}
-
-
 
 
 export const SignInForm = (props) => {
     const [username, setUsername] = React.useState("")
     const [forgotPassword, setForgotPassword] = React.useState(false)
     const [forgotPasswordConfirm, setForgotPasswordConfirm] = React.useState(false)
+    const [errorMsg, setErrorMsg] = React.useState(() => null)
 
-    const handleSubmit = (event) => {
-        signIn({ un: event.target[0].value, pw: event.target[2].value }).then(response => props.closeMenu)
-        event.preventDefault()
-    }
+    async function signIn(props) { try { await Auth.signIn(props.un, props.pw); setErrorMsg(null) } catch (error) { setErrorMsg(error.message); console.log('error signing in', error) } }
+    async function forgotPd(props) { try { await Auth.forgotPassword(props.un); setErrorMsg(null) } catch (error) { setErrorMsg(error.message); console.log('error with forgot password', error) } }
+    async function submitForgotPassword(props) { try { Auth.forgotPasswordSubmit(props.un, props.code, props.new_pw); setErrorMsg(null) } catch (error) { setErrorMsg(error.message); console.log('error forgot password submit', error) } }
+    const handleSubmit = (event) => { signIn({ un: event.target[0].value, pw: event.target[2].value }).then(response => props.closeMenu); event.preventDefault() }
+    const handleReset = (event) => { event.preventDefault(); forgotPd({ un: event.target[0].value }).then(respnse => setForgotPasswordConfirm(true)) }
+    const handleResetConfirm = (event) => { submitForgotPassword({ un: event.target[0].value, code: event.target[2].value, new_pw: event.target[4].value }).then(response => (setForgotPasswordConfirm(false), setForgotPassword(false))); event.preventDefault() }
 
-    const handleReset = (event) => {
-        event.preventDefault()
-        forgotPd({ un: event.target[0].value })
-            .then(respnse => setForgotPasswordConfirm(true))
-    }
-
-    const handleResetConfirm = (event) => {
-        submitForgotPassword({ un: event.target[0].value, code: event.target[2].value, new_pw: event.target[4].value })
-            .then(response => (
-                setForgotPasswordConfirm(false),
-                setForgotPassword(false)
-            ))
-        event.preventDefault()
-    }
-
-
-    const paperStyle = {
-        padding: 20,
-        height: 'auto',
-        width: '25rem',
-        maxWidth: '90vw',
-        // margin: '3rem auto',
-        border: "2px solid #2e2e2e",
-        borderRadius: "15px",
-    }
-    const gridStyle = {
-        backgroundColor: "black",
-    }
+    const paperStyle = { padding: 20, height: 'auto', width: '25rem', maxWidth: '90vw', border: "2px solid #2e2e2e", borderRadius: "15px" }
+    const gridStyle = { backgroundColor: "black" }
     const buttonStyle = { margin: '1rem 0', color: "black" }
-
 
     return (
         <ThemeProvider>
@@ -114,25 +43,8 @@ export const SignInForm = (props) => {
                                 <Typography color="white" fontSize="clamp(24px, 4vw, 36px)" fontWeight="bold">Sign In</Typography>
                             </Grid>
                             <form onSubmit={handleSubmit}>
-                                <TextFields
-                                    style={buttonStyle}
-                                    // label="Email"
-                                    placeholder="Enter email"
-                                    fullWidth
-                                    required
-                                    inputProps={{ sx: { "&::placeholder": { color: "white" } } }}
-                                    sx={{ input: { color: 'white' } }}
-                                />
-                                <TextFields
-                                    style={buttonStyle}
-                                    // label="Password"
-                                    placeholder="Enter password"
-                                    type='password'
-                                    fullWidth
-                                    required
-                                    inputProps={{ sx: { "&::placeholder": { color: "white" } } }}
-                                    sx={{ input: { color: 'white' } }}
-                                />
+                                <TextFields style={buttonStyle} placeholder="Enter email" fullWidth required inputProps={{ sx: { "&::placeholder": { color: "white" } } }} sx={{ input: { color: 'white' } }} />
+                                <TextFields style={buttonStyle} placeholder="Enter password" type='password' fullWidth required inputProps={{ sx: { "&::placeholder": { color: "white" } } }} sx={{ input: { color: 'white' } }} />
                                 <SignInButton type='submit' variant='contained' fullWidth>Sign In</SignInButton>
                             </form>
                             <Typography>
@@ -152,7 +64,6 @@ export const SignInForm = (props) => {
                                 <form onSubmit={handleReset}>
                                     <TextFields
                                         style={buttonStyle}
-                                        // label="Email"
                                         placeholder="Enter email"
                                         fullWidth
                                         required
@@ -199,6 +110,11 @@ export const SignInForm = (props) => {
                         Don&apos;t have an account? &nbsp;
                         <Link href="#" onClick={props.signUpForm} color="inherit" sx={{ '&:hover': { color: '#dd00ff' } }}>Sign up here</Link>
                     </Typography>
+                    {errorMsg &&
+                        <Grid item marginBottom={1} marginTop={1}>
+                            <Typography sx={{ color: "#FF0000" }}> {errorMsg} </Typography>
+                        </Grid>
+                    }
                 </Paper>
             </Grid>
         </ThemeProvider>
